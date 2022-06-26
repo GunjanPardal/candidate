313ba6b5d351fadaa0cacd276156dbe803147324
Gunjan Pardal <pardalgunjan@gmail.com>
2022-06-26 11:40:47 +0530

    candidate changes

---
+++ src/main/java/com/workday/java/JobRunnerImpl.java
@@ -0,0 +1,20 @@
+package com.workday.java;
+
+/**
+ *
+ * Your code goes here
+ *
+ */
+
+public class JobRunnerImpl implements JobRunner {
+
+    @Override
+    public void run(JobQueue jobQueue) {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
